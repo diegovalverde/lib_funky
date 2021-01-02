3b13caaac59116e26ba5dece864f2742ec160ece
diegovalverde <diego.valverde.g@gmail.com>
2021-01-02 18:25:36 +0000

    improved error reporting

--- funk_utils.py
+++ funk_utils.py
@@ -1,5 +1,11 @@
 import re
 from lark import Lark
+from lark.exceptions import UnexpectedEOF, UnexpectedToken
 def formatCompilationError(e):
-    
-    return 'Syntax error row:{} col:{}\n'.format(e.line,  e.column)
+    if (isinstance(e, UnexpectedEOF)):
+        return 'Unexpected end of file'
+    elif (isinstance(e, Exception)):
+        return e.__str__()
+    else:
+        return 'Syntax error row:{} col:{}\n'.format(e.line,  e.column)
+
