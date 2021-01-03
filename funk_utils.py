ed9cfd9d4d68c284fd71b70be563e75c2778581b
diegovalverde <diego.valverde.g@gmail.com>
2021-01-03 00:42:25 +0000

    @cosmetic

--- funk_utils.py
+++ funk_utils.py
@@ -1,10 +1,10 @@
 import re
 from lark import Lark
-from lark.exceptions import UnexpectedEOF, UnexpectedToken
+from lark.exceptions import UnexpectedEOF, UnexpectedToken, UnexpectedCharacters
 def formatCompilationError(e):
     if (isinstance(e, UnexpectedEOF)):
         return 'Unexpected end of file'
-    elif (isinstance(e, Exception)):
-        return e.__str__()
-    else:
+    elif isinstance(e,UnexpectedToken) or isinstance(e,UnexpectedCharacters):
         return 'Syntax error row:{} col:{}\n'.format(e.line,  e.column)
+    else:
+        return '>>>>>>' + e.__str__()
