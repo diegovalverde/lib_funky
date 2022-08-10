c088ebc8adc259f7b37bc5c2a824b57526d96100
diego.valverde <diego.valverde.g@gmail.com>
2022-08-10 18:58:22 +0200

    implementing js emitter

--- funky_emitter_js.py
+++ funky_emitter_js.py
@@ -103,7 +103,7 @@ class EmitterJs:
 
                 }}
 
-                if ({name}.fn == null){{
+                if ({name}.data == null){{
                     funky_console.value +="FunkyRuntime Error: '{name}' function is NULL <br\>";
 
                 }}
